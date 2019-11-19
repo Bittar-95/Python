6991b5122113f777bbedd30dec8831d4b1685970
Mohammad <47321335+Bittar-95@users.noreply.github.com>
2019-11-19 15:32:05 +0200

    Add files via upload

---
+++ PythonSourceCode.py
@@ -0,0 +1,104 @@
+#Q1
+#print("Hello\t")
+#print("\tHello")
+#print("\t\tHello")
+##2
+#print("Orange\n"*20)
+##3
+#x=float(1);
+#y=float(2.8);
+#z=float("3")
+#w=float("4.2")
+#print(x,y,z,w,sep='  ');
+##4
+#numberOne = int(input("Number One :"));
+#numberTwo =int(input("Number Two :"));
+#print(numberOne * numberTwo);
+
+#5
+#grades = [];
+#counter = 0;
+#sum = 0;
+#while (counter <5) :
+#    grades.append( input("Enter Grade "+str(counter + 1)+":"))
+#    sum = sum + int(grades[counter]);
+#    counter = counter+1;
+#print(sum/len(grades));
+
+#6
+#x=1;
+#y=2.8;
+#z=1j;
+#o="orange";
+#A=True;
+#print(type(x))
+#print(type(y))
+#print(type(z))
+#print(type(o))
+#print(type(A))
+
+#7
+#x,y = "orange",1;
+#X1,Y1 = 100, -10;
+#print(x)
+#print(y)
+#print(X1)
+#print(Y1)
+
+#8
+#5x=10; #Erro
+#print(5x) #Erro
+#X_q$ = "Orange"; #Error
+#print(X_q$) #Error
+#A B = 14 #Error
+#print(A B)#Error
+#print("10" * 10)
+
+#9
+#x,y = "orange" , 1;
+#X1,y1 = 100 , -10;
+#print(x)
+#print(y)
+#print(X1)
+#print(y1)
+
+
+#11
+#print("Hello World")
+#print("Cheers,Mate!");
+""" Testing
+The Code
+"""
+#12
+#name = input("Your name ?");
+#age = int(input("Your Age ?"));
+#currentYear = 2019;
+#print("Year :",(currentYear - age) + 100);
+
+#13
+#height = int(input("Enter Traiangle Height:"));
+#base = int(input("Enter Trainangle Base:"));
+#print(0.5 * base * height);
+
+#14
+#x= 11;
+#y=3;
+#print(x+y)
+#print(x-y)
+#print(x*y)
+#print(x/y)
+#print(x//y)
+#print(x%y)
+#print(abs(x*-1))
+#print(int(x))
+#print(float(x))
+#print(divmod(x,y))
+#print(pow(x,y))
+#print(x**y)
+#print(x>y)
+#print(x==y)
+#print(x!=y)
+
+
+
+
