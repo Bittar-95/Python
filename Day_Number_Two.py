17a71c029c6b5e4ee92343f66deb3f0a864800fd
Bittar-95 <mohammad.y.albittar@gmail.com>
2019-11-20 17:03:42 +0200

    Exercise Number Two

---
+++ Day_Number_Two.py
@@ -0,0 +1,98 @@
+#Q1---------------------------------------------
+#numberOne= int(input("Please Inout Number One: "));
+#numberTwo= int(input("Please Inout Number Two: "));
+#print(str(numberTwo)) if numberTwo>numberOne else print(str(numberOne) + "=" + str(numberTwo)) if (numberOne==numberTwo) else print(str(numberOne))
+#---------------------------------------------
+
+#Q2---------------------------------------------
+#productNumber = int(input("Please Enter Product Number"));
+#for result in range(0,11):
+#    value = productNumber * result;
+#    print(str(result) + "*"+str(productNumber) + "= " + str(value));
+#---------------------------------------------
+
+#Q3---------------------------------------------
+    
+end = int(input("Please Enter The Number Of Lines"));
+start = 0
+increment = 1
+countre = end//2
+for line in range(start,end,increment):
+    for stars in range(line):
+        print("*", end=" ")
+        
+    if (line == countre):
+         start=line
+         end = 0
+         increment=-1
+    print("\n")
+
+    
+    
+#---------------------------------------------
+
+#Q4---------------------------------------------
+    
+    #x
+    #y
+    #z
+    #b
+    
+    
+#---------------------------------------------
+
+#Q5---------------------------------------------
+    
+    #12
+    #15
+    #18
+    #21
+    #24
+    
+#---------------------------------------------
+
+
+#Q6---------------------------------------------
+    
+    #1
+    #2
+    #3
+    
+    
+#---------------------------------------------
+
+#Q7---------------------------------------------
+#yourNumber = int(input("Please Enter you Number": ));
+#summation = 0;
+#for number in range(yourNumber+1):
+#        summation+=number;
+#print(summation);
+        
+#---------------------------------------------
+
+#Q8---------------------------------------------
+#signNumber ="Even" if (int(input("Enter The Number: "))%2==0) else "Odd";
+#print(signNumber);
+#---------------------------------------------
+
+#Q9---------------------------------------------
+#Patterns Number Nine
+    
+#---------------------------------------------
+   
+#Q10---------------------------------------------
+#while True:
+#    try:
+#        number = int(input("Enter Your Number : "));
+#        print("Your Number Is :" , number);
+#        break;
+#    except:
+#        print("Please  Enter Real Number")
+    
+#---------------------------------------------
+    
+    
+#Q11---------------------------------------------
+#    Error Occured And Handler
+    
+#---------------------------------------------
